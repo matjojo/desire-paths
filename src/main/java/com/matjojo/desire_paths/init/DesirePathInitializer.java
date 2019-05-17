bd80136d4b7b8ea9936caf090d54ef4a68647c88
matjojo <mat2904jojo@gmail.com>
2019-05-17 15:51:53 +0200

    Blocks now drop like their original block (Solves #10)

--- src/main/java/com/matjojo/desire_paths/init/DesirePathInitializer.java
+++ src/main/java/com/matjojo/desire_paths/init/DesirePathInitializer.java
@@ -25,10 +25,10 @@ import net.minecraft.util.registry.Registry;
 
 public class DesirePathInitializer implements ModInitializer {
 
-    public static final Block DIRT_COARSE_INTER = new Trampleable(FabricBlockSettings.copy(Blocks.DIRT).ticksRandomly().build());
-    public static final Block GRASS_DIRT_INTER = new Trampleable(FabricBlockSettings.copy(Blocks.DIRT).ticksRandomly().build());
-    public static final Block PODZOL_DIRT_INTER = new Trampleable(FabricBlockSettings.copy(Blocks.DIRT).ticksRandomly().build());
-    public static final Block MYCELIUM_DIRT_INTER = new Trampleable(FabricBlockSettings.copy(Blocks.DIRT).ticksRandomly().build());
+    public static final Block DIRT_COARSE_INTER = new Trampleable(FabricBlockSettings.copy(Blocks.DIRT).ticksRandomly().dropsLike(Blocks.DIRT).build());
+    public static final Block GRASS_DIRT_INTER = new Trampleable(FabricBlockSettings.copy(Blocks.DIRT).ticksRandomly().dropsLike(Blocks.GRASS_BLOCK).build());
+    public static final Block PODZOL_DIRT_INTER = new Trampleable(FabricBlockSettings.copy(Blocks.DIRT).ticksRandomly().dropsLike(Blocks.PODZOL).build());
+    public static final Block MYCELIUM_DIRT_INTER = new Trampleable(FabricBlockSettings.copy(Blocks.DIRT).ticksRandomly().dropsLike(Blocks.MYCELIUM).build());
 
     @Override
     public void onInitialize()
